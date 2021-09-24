27bb03b206da464f98481e746866adecbe732389
Mark Learst <learst@me.com>
2021-09-24 14:05:33 -0400

    Fixed type errors and updated buttons

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,6 +1,15 @@
 // File: src/buttons/Button.tsx
 
-import React from 'react';
+import React from 'react'
 
-export const Button: React.FC = () =>
-  <button>Hello World</button>;
+export const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<'button'>
+>(({ children, ...props }, ref,) => (
+  <button ref={ref} type="button" {...props}>
+    {children}
+  </button>
+)
+);
+
+Button.displayName = 'Button';
