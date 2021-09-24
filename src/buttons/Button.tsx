ace95cde635e87e92ce19d2fe49c08ed4d478336
Mark Learst <learst@me.com>
2021-09-24 12:10:06 -0400

    chore() Renamed component

---
+++ src/buttons/Button.tsx
@@ -0,0 +1,5 @@
+// File: src/buttons/Button.tsx
+
+import React from 'react'
+
+export const Button = () => <button>Hello World</button>
--- src/buttons/Buttons.tsx
+++
@@ -1,7 +0,0 @@
-// File: src/buttons/Button.tsx
-
-import React from 'react'
-
-const Button = () => <button>Hello World</button>
-
-export default Button
